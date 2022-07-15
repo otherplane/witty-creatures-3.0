aed315f13e5e88f9588a84477b9455001bc2f3e2
Tomás Ruiz <tomastrg22@gmail.com>
2022-07-15 19:19:44 +0200

    fix(ui): remove extra / in backup link

--- packages/ui/src/services/exportInformation.js
+++ packages/ui/src/services/exportInformation.js
@@ -4,10 +4,10 @@ export function createImportLink() {
   const { key, username, token } = JSON.parse(localStorage.getItem('tokenInfo'))
   const mintInfo = JSON.parse(localStorage.getItem('mintInfo'))
   if (mintInfo?.mintConfirmation) {
-    return `${BASE_URL}/#/import?key=${key}&username=${username}&token=${token}&txHash=${mintInfo.txHash}&from=${mintInfo.from}&mintConfirmation=${mintInfo.mintConfirmation}`
+    return `${BASE_URL}#/import?key=${key}&username=${username}&token=${token}&txHash=${mintInfo.txHash}&from=${mintInfo.from}&mintConfirmation=${mintInfo.mintConfirmation}`
   } else if (mintInfo?.txHash) {
-    return `${BASE_URL}/#/import?key=${key}&username=${username}&token=${token}&txHash=${mintInfo.txHash}&from=${mintInfo.from}`
+    return `${BASE_URL}#/import?key=${key}&username=${username}&token=${token}&txHash=${mintInfo.txHash}&from=${mintInfo.from}`
   } else {
-    return `${BASE_URL}/#/import?key=${key}&username=${username}&token=${token}`
+    return `${BASE_URL}#/import?key=${key}&username=${username}&token=${token}`
   }
 }
