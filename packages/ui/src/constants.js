9d4d0c1c33ad600405c0bef8b46c835e9b0a6cd6
gabaldon <maria@otherplane.com>
2022-07-15 10:27:06 +0200

    fix(ui): change default base url

--- packages/ui/src/constants.js
+++ packages/ui/src/constants.js
@@ -14,7 +14,8 @@ export const VITE_TEST = import.meta.env.VITE_TEST || false
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || 'http://0.0.0.0:4000'
 
-export const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000'
+export const BASE_URL =
+  import.meta.env.VITE_BASE_URL || 'https://wittycreatures.com/'
 
 export const ATTRIBUTES = {}
 
